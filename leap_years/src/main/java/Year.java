ebe05fe5ad380ca137a41c937b4dfe05b5694a93
Delfland <d_dallison@hotmail.com>
2023-03-21 14:47:55 +0000

    started first kata LeapYear in java

---
+++ leap_years/src/main/java/Year.java
@@ -0,0 +1,10 @@
+public class Year {
+
+    int year;
+    public Year(int year) {
+        this.year = year;
+    }
+
+    public boolean checkLeap() {
+    }
+}
---
+++ leap_years/src/test/java/YearTests.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class YearTests {
+
+    Year year;
+
+    @Test
+    public void allYearsDivisibleBy400AreLeapYears() {
+        year = new Year(2000);
+        assertEquals(true, year.checkLeap());
+    }
+
+    @Test
+    public void allYearsDivisibleBy100ButNotBy400AreNotLeapYears() {
+        year = new Year(1700);
+        assertEquals(false, year.checkLeap());
+    }
+
+    @Test
+    public void allYearsDivisibleBy4ButNotBy100AreLeapYears() {
+        year = new Year(2008);
+        assertEquals(true, year.checkLeap());
+    }
+
+    @Test
+    public void allYearsNotDivisibleBy4AreNotLeapYears() {
+        year = new Year(2017);
+        assertEquals(true, year.checkLeap());
+    }
+
+
+
+}
